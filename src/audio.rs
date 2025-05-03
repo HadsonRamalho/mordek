3b3e1789d0d82323bd5554518e2c320a82c05338
MauraGusmao <mauragusmaoneta123456@gmail.com>
2025-05-03 12:04:01 -0300

    feat: iniciando repositório

---
+++ src/audio.rs
@@ -0,0 +1,14 @@
+use std::fs::File;
+use std::io::BufReader;
+use rodio::{Decoder, OutputStream, source::Source};
+
+pub fn reproduzir_audio(){
+    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+
+    let file = BufReader::new(File::open("C:\\Users\\maura\\Documents\\kaiser-rust\\target\\release\\sucumba.mp3").unwrap());
+
+    let source = Decoder::new(file).unwrap();
+    stream_handle.play_raw(source.convert_samples());
+
+    std::thread::sleep(std::time::Duration::from_secs(3));
+}
